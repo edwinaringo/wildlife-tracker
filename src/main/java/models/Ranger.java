70c57f48d115b53f9015637811e10335476eceee
edwinaringo <tedaringo628@gmail.com>
2021-05-02 23:09:43 +0300

    Adding getters and setters on the Rangers class

--- src/main/java/models/Ranger.java
+++ src/main/java/models/Ranger.java
@@ -1,4 +1,20 @@
 package models;
 
 public class Ranger {
+    private String name;
+    private int id;
+
+    public Ranger(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
