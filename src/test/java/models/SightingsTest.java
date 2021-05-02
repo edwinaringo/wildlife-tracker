c65e4d78aca03d34329ff18badd2951490caf744
edwinaringo <tedaringo628@gmail.com>
2021-05-02 23:44:33 +0300

    Generating test to check instantiation

--- src/test/java/models/SightingsTest.java
+++ src/test/java/models/SightingsTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +14,15 @@ class SightingsTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    private Sightings otherSighting(){
+        return new Sightings("Lion","West Wing",1);
+    }
+
+    @Test
+    public void sightings_canInstantiateCorrectly_boolean(){
+        Sightings sightings = otherSighting();
+        assertTrue(sightings instanceof Sightings);
     }
 }
