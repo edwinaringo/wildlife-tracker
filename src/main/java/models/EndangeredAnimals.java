e878d9466c838abca0535dda95e8101d0c8c734f
edwinaringo <tedaringo628@gmail.com>
2021-05-02 22:23:33 +0300

    Adding properties to the EndangeredAnimals class

--- src/main/java/models/EndangeredAnimals.java
+++ src/main/java/models/EndangeredAnimals.java
@@ -1,4 +1,12 @@
 package models;
 
-public class EndangeredAnimals {
+public class EndangeredAnimals extends Animals{
+    private static final String DB_TYPE = "Endangered";
+
+    public EndangeredAnimals(String name, String health, String age) {
+        this.name = name;
+        this.health = health;
+        this.age = age;
+        this.type = DB_TYPE;
+    }
 }
