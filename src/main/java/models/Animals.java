55a49cc6b3b108a9891d55a3077bdd763394fa91
edwinaringo <tedaringo628@gmail.com>
2021-05-02 21:41:42 +0300

    Generating getters and setters for animal class

--- src/main/java/models/Animals.java
+++ src/main/java/models/Animals.java
@@ -6,4 +6,43 @@ public class Animals {
     public String age;
     public String health;
     public String type;
+
+    //getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
 }
