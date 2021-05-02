bbdf863797703c79b7c6972cfbd57ca97d519bca
edwinaringo <tedaringo628@gmail.com>
2021-05-02 23:12:35 +0300

    Generating test to check instantiation

--- src/test/java/models/RangerTest.java
+++ src/test/java/models/RangerTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,4 +15,16 @@ class RangerTest {
     @AfterEach
     void tearDown() {
     }
+
+    private Ranger otherRanger(){
+        return new Ranger("Nike",2);
+    }
+
+    @Test
+    public void rangerClass_canInstantiateCorrectly(){
+        Ranger ranger = otherRanger();
+        assertTrue(ranger instanceof Ranger);
+    }
+
+
 }
