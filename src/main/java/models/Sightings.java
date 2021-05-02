7ac2a557bc4645aa747953f2aac3f5294d2eb368
edwinaringo <tedaringo628@gmail.com>
2021-05-02 23:25:54 +0300

    Adding a constructor to the sightings class

--- src/main/java/models/Sightings.java
+++ src/main/java/models/Sightings.java
@@ -5,6 +5,13 @@ public class Sightings {
     private int rangerid;
     private String location;
     private int id;
+
+    public Sightings(String animalName, String location, int rangerid) {
+        this.animalName = animalName;
+        this.location = location;
+        this.rangerid = rangerid;
+        this.id = rangerid;
+    }
 }
 
 
